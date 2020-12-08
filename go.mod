af3544e75aeb596bd0cd28008089065ef5785c40
Juuhabach <173709@upt.br>
2020-12-08 15:45:52 -0300

    Commit 03

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module Hexagonal-MSs-01
 go 1.15
 
 require (
-	github.com/Juuhabach/Hexagonal-MSs-01 v0.0.0-20201205232059-d920abc8430e // indirect
+	github.com/go-chi/chi v1.5.1
 	github.com/pkg/errors v0.9.1
 	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	go.mongodb.org/mongo-driver v1.4.4
+	google.golang.org/appengine v1.6.7 // indirect
 	gopkg.in/dealancer/validate.v2 v2.1.0
 )
--- main.go
+++ main.go
@@ -1,5 +1,78 @@
 package main
 
-func main() {
+import (
+	h "Hexagonal-MSs-01/API"
+	mr "Hexagonal-MSs-01/repository/mongo"
+	"Hexagonal-MSs-01/shortener"
+	//cr "Hexagonal-MSs-01/repository/cassandra"
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+)
 
+func main() {
+	repo := chooseRepo()
+	service := shortener.NewRedirectService(repo)
+	handler := h.NewHandler(service)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{code}", handler.Get)
+	r.Post("/", handler.Post)
+
+	errs := make(chan error, 2)
+	go func() {
+		fmt.Println("Listening on Port:8000")
+		errs <- http.ListenAndServe(httpPort(), r)
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	fmt.Printf("Terminated %s", <-errs)
+}
+
+func httpPort() string {
+	port := "8000"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	return fmt.Sprintf(":%s", port)
+}
+
+func chooseRepo() shortener.RedirectRepository {
+	switch os.Getenv("PORT") {
+	/*case "cassandra":
+	cassandraURL    := os.Getenv("CASSANDRA_URL")
+	cassandradb     := os.Getenv("CASSANDRA_DB")
+	cassandraTimeout:= os.Getenv("CASSANDRA_TIMEOUT")
+	repo, err := cr.NewCassandraRepository(cassandraURL, cassandradb, cassandraTimeout)
+	if err == nil {
+		log.Fatal(err)
+	}
+	return repo*/
+	case "mongo":
+		mongoURL := os.Getenv("MONGO_URL")
+		mongodb := os.Getenv("MONGO_DB")
+		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
+		repo, err := mr.NewMongoRepository(mongoURL, mongodb, mongoTimeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	}
+	return nil
 }
